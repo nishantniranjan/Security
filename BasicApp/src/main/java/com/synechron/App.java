88ea2aec49869507ef8478ab2ebcb5cd17c8f2f3
Nishant Niranjan <nishant.niranjan@gmail.com>
2015-03-29 15:04:42 +0100

    adding basic application

--- BasicApp/src/main/java/com/synechron/App.java
+++ BasicApp/src/main/java/com/synechron/App.java
@@ -1,5 +1,10 @@
 package com.synechron;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.synechron.model.Person;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+        Person person = (Person) ctx.getBean("personBean");
+        person.setName("Nishant Niranjan");
+        System.out.println("Person::"+person);
     }
 }
---
+++ BasicApp/src/main/java/com/synechron/model/Person.java
@@ -0,0 +1,30 @@
+package com.synechron.model;
+
+public class Person {
+	
+	private String name;
+	
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Person(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + "]";
+	}
+	
+}
